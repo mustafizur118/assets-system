ad69e12eaac391759e351651829b0601c1fad993
Mustafizur Rahman <mr2480379@gmail.com>
2022-11-12 20:24:29 +0600

    assets system done

---
+++ asset_app/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+ASSET_CHOICES = (
+    ("Desk", "Desk"),
+    ("Laptop", "Laptop"),
+    ("IP Phone", "IP Phone"),
+    ("Project", "Project"),
+)
+
+LOCATION_CHOICES = (
+    ("Ground Floor", "Ground Floor"),
+    ("Mezzanine", "Mezzanine"),
+    ("Second Floor", "Second Floor"),
+    ("Meeting Room", "Meeting Room"),
+)
+class Assets(models.Model):
+    asset_name = models.CharField(max_length=100, choices=ASSET_CHOICES)
+    asset_serial_No = models.CharField(max_length=100)
+    asset_manufacturer = models.CharField(max_length=100)
+    date_purchased = models.DateTimeField()
+    asset_issued = models.BooleanField(default=False)
+    asset_image = models.ImageField(default="default.jpeg", upload_to = 'images/')
+
+    def __str__(self):
+        return self.asset_name
+
+    def get_absolute_url(self):
+        return reverse('assets-detail', kwargs={'pk': self.pk})
+
+
+class AssetsIssuance(models.Model):
+    asset=models.ForeignKey(Assets,on_delete=models.PROTECT)
+    asset_location = models.CharField(max_length=100, choices=LOCATION_CHOICES)
+    date_issued = models.DateTimeField(default=timezone.now)
+    asset_assignee = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.asset
+
+    def get_absolute_url(self):
+        return reverse('assets-detail', kwargs={'pk': self.pk})
+
+
+class Post:
+    pass
+
+
+class Neighborhood:
+    pass
+
+
+class Business:
+    pass
+
+
+class Contact:
+    pass
---
+++ asset_app/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.urls import re_path as url
+from .views import (
+    AssetsListView,
+    AssetsDetailView,
+    AssetsCreateView,
+    AssetsUpdateView,
+    AssetsDeleteView,
+)
+from . import views
+
+urlpatterns = [
+    path('', AssetsListView.as_view(), name='assets_app_home'),
+    path('assets/<int:pk>/', AssetsDetailView.as_view(), name='assets-detail'),
+    path('assets/new/', AssetsCreateView.as_view(), name='assets-create'),
+    path('assets/<int:pk>/update/', AssetsUpdateView.as_view(), name='assets-update'),
+    path('assets/<int:pk>/delete/', AssetsDeleteView.as_view(), name='assets-delete'),
+    path('about/', views.about, name='asset_app-about'),
+    url(r'^searchassets/$', views.assetssearch, name='assetssearch'),
+]
